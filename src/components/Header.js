8056ff7afe8a11d652195a2814a9c44d31ef037a
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-02-08 19:37:02 +0100

    create the Header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+    return ( 
+        <header>
+            <div className="header-bg">
+                <h2>Welcome To The Pets Health Blog!</h2>
+                <p>The place where you will find out all about the needs of your beloved pet</p>
+            </div>
+        </header>
+     );
+}
+ 
+export default Header;

91cd16cf128be0f289f5223ffd0fc97c127a0518
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-02-08 18:59:35 +0100

    create the Navigation and Footer components

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import { FaRegCopyright } from 'react-icons/fa'
+
+const Footer = () => {
+    return ( 
+        <footer>
+            <p><FaRegCopyright /> The Pets Health Blog 2021</p>
+        </footer>
+     );
+}
+ 
+export default Footer;

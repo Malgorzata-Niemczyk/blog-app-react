490b48fb299bc03cfcc6bcb5702ae6541fa3fee5
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-02-08 20:27:06 +0100

    add additional classes in the Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 const Header = () => {
     return ( 
         <header>
-            <div className="header-bg">
-                <h2>Welcome To The Pets Health Blog!</h2>
-                <p>The place where you will find out all about the needs of your beloved pet</p>
+            <div className="header__bg">
+                <div className="hero__bg"></div>
+                <div className="header__text">
+                    <h2>Welcome To The Pets Health Blog!</h2>
+                    <p>The place where you will find out all about the needs of your beloved pet</p>
+                </div>
             </div>
         </header>
      );

0ff92dc9f43d20ad78524303110e9e14ec0e9ed7
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-02-08 18:45:56 +0100

    import the icon from react-icons and insert it within the Navigation component

--- src/assets/components/Navigation.js
+++ src/assets/components/Navigation.js
@@ -1,7 +1,9 @@
+import { FaPaw } from 'react-icons/fa';
+
 const Navigation = () => {
     return ( 
         <div className="navbar">
-            <h1>The Pets Health Blog</h1>
+            <h1><FaPaw /> The Pets Health Blog</h1>
             <div className="navbar__links">
                 <a href="">Home</a>
                 <a href="" className="navbar__links__new__blog">New Blog</a>

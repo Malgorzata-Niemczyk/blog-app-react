ac9a47eb31958222084d42a45a5191c814da08a5
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-02-09 18:40:53 +0100

    correct an array of objects in terms of punctuation and update the the image key paths; use the map() method to map through the blogs objects to display them on the webpage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,49 +1,56 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {
-            "image": "../assets/img/dog-img.jpg", 
-            "title": "How to Enrich Your Dog's Life Starting Today!",
-            "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat...",
-            "author": "Jane Doe",
-            "id": 1
-        },
-        {
-            "image": "../assets/img/cats-img.jpg",
-            "title": "12 Tips for Raising a Healthy, Happy Kitten",
-            "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat...",
-            "author": "John White",
-            "id": 2
-        },
-        {
-            "image": "../assets/img/hamster-img.jpg",
-            "title": "Hamster Diet Tip - Hamster Fresh Food Guide!",
-            "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat...",
-            "author": "Lauren Smith",
-            "id": 3
-        },
-        {
-            "image": "../assets/img/rabbit-img.jpg",
-            "title": "5 Ways to Care for an Older Rabbit",
-            "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat...",
-            "author": "Jane Doe",
-            "id": 4
-        },
-        {
-            "image": "../assets/img/dog-food-img.jpg",
-            "title": "Factors to Consider When Choosing the Best Food for Your Pet",
-            "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat...",
-            "author": "John White",
-            "id": 5
-        }
-    ]);
+  const [blogs, setBlogs] = useState([
+    {
+        image: 'https://cdn.pixabay.com/photo/2018/04/23/14/38/adorable-3344414_1280.jpg', 
+        title: `How to Enrich Your Dog's Life Starting Today!`,
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+        author: 'Jane Doe',
+        id: 1
+    },
+    {
+        image: 'https://cdn.pixabay.com/photo/2019/06/18/11/32/cat-4282123_1280.jpg',
+        title: '12 Tips for Raising a Healthy, Happy Kitten',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+        author: "John White",
+        id: 2
+    },
+    {
+        image: 'https://cdn.pixabay.com/photo/2020/05/01/02/26/hamster-5115246_1280.jpg',
+        title: 'Hamster Diet Tip - Hamster Fresh Food Guide!',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+        author: 'Lauren Smith',
+        id: 3
+    },
+    {
+        image: 'https://cdn.pixabay.com/photo/2018/09/29/21/18/rabbit-3712339_1280.jpg',
+        title: '5 Ways to Care for an Older Rabbit',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+        author: 'Jane Doe',
+        id: 4
+    },
+    {
+        image: 'https://cdn.pixabay.com/photo/2020/05/13/21/03/dog-food-5168940_1280.jpg',
+        title: 'Factors to Consider When Choosing the Best Food for Your Pet',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+        author: 'John White',
+        id: 5
+    }
+  ])
 
-    return ( 
+    return (
         <div className="home">
-
+            {blogs.map(blog => (
+                <div className="blog__preview" key={blog.id} >
+                    <img src={ blog.image } />
+                    <h2>{ blog.title }</h2>
+                    <p>{ blog.body }</p>
+                    <p>Written by { blog.author }</p>
+                </div>
+            ))}
         </div>
-     );
+    );
 }
  
 export default Home;

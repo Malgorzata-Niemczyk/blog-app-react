6b15273a6b91e905b4fa8f1b0809ac156a951a6c
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-02-12 00:08:48 +0100

    create another state to output a loading message  on a webpage but only when the data has not been fetched yet

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,16 +2,21 @@ import { useState, useEffect } from "react";
 import BlogsList from "./BlogsList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null)
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
             .then(res => res.json())
-            .then(data => setBlogs(data))
+            .then(data => {
+                setBlogs(data);
+                setIsPending(false);
+            })
     }, [])
 
     return (
         <div className="home">
+            {isPending && <div className="loading__message">Loading...</div>}
             {blogs && <BlogsList blogs={blogs} title="All Our Blogs!" />}
         </div>
     );

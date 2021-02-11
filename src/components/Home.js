327b741d3a75826a99ed2cf15e6d1f41c002d56a
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-02-12 00:41:21 +0100

    add another useState hook for throwing an error; in the fetch request add an if statement that throws an error on the webpage when the status of the response is not equal to 200; the error is set back to null so it does not appears on the webpage when the data is fetched

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,18 +4,30 @@ import BlogsList from "./BlogsList";
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
-            .then(res => res.json())
+            .then(res => {
+                if (res.status !== 200) {
+                    throw Error('Sorry, could not fetch the data for that resource :(')
+                }
+                return res.json()
+            })
             .then(data => {
                 setBlogs(data);
                 setIsPending(false);
+                setError(null);
             })
-    }, [])
+            .catch(err => {
+                setError(err.message);
+                setIsPending(false);
+            })
+    }, []);
 
     return (
         <div className="home">
+            {error && <div className="error__message">{ error }</div>}
             {isPending && <div className="loading__message">Loading...</div>}
             {blogs && <BlogsList blogs={blogs} title="All Our Blogs!" />}
         </div>

f439cb7c7e82c2e360afc7c011b58cdd1feb54b0
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-02-12 02:28:39 +0100

    delete the imported hooks; destructure the  custom hook of useFetch to get the access to the useState hooks to fetch the blogs data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,14 @@
-import { useState, useEffect } from "react";
 import BlogsList from "./BlogsList";
+import useFetch from "./useFetch";
 
 const Home = () => {
+    const { data, isPending, error } = useFetch('http://localhost:8000/blogs');
+
     return (
         <div className="home">
             {error && <div className="error__message">{ error }</div>}
             {isPending && <div className="loading__message">Loading...</div>}
-            {blogs && <BlogsList blogs={blogs} title="All Our Blogs!" />}
+            {data && <BlogsList blogs={data} title="All Our Blogs!" />}
         </div>
     );
 }

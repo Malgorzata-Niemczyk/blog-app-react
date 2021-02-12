8f4ec007264737fbf03f53a5b49feb601db20e44
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-02-12 01:19:52 +0100

    remove the useEffect hook and the useState hooks from the Home component and paste this content into a useFetch.js file

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,29 +2,6 @@ import { useState, useEffect } from "react";
 import BlogsList from "./BlogsList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then(res => {
-                if (res.status !== 200) {
-                    throw Error('Sorry, could not fetch the data for that resource :(')
-                }
-                return res.json()
-            })
-            .then(data => {
-                setBlogs(data);
-                setIsPending(false);
-                setError(null);
-            })
-            .catch(err => {
-                setError(err.message);
-                setIsPending(false);
-            })
-    }, []);
-
     return (
         <div className="home">
             {error && <div className="error__message">{ error }</div>}
--- src/components/useFetch.js
+++ src/components/useFetch.js
@@ -1,1 +1,26 @@
 // creating a custom hook (each custom hook needs to start with a word 'use', e.g useFetch to make it work)
+
+const  useFetch = () => {
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                if (res.status !== 200) {
+                    throw Error('Sorry, could not fetch the data for that resource :(')
+                }
+                return res.json()
+            })
+            .then(data => {
+                setBlogs(data);
+                setIsPending(false);
+                setError(null);
+            })
+            .catch(err => {
+                setError(err.message);
+                setIsPending(false);
+            })
+    }, []);
+}

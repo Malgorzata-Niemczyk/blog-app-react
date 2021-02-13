a34f2d0bfcc01233da4c3216c1f419660ec062be
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-02-13 01:12:23 +0100

    import the Link component from the react-router-dom package and add this component in the JSX template instead of the anchor tags to enable switching the components on the page

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,13 @@
 import { FaPaw } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
     return ( 
         <div className="navbar">
             <h1><FaPaw /> The Pets Health Blog</h1>
             <div className="navbar__links">
-                <a href="">Home</a>
-                <a href="" className="navbar__links__new__blog">New Blog</a>
+                <Link to="/">Home</Link>
+                <Link to="/create-blog" className="navbar__links__new__blog">New Blog</Link>
             </div>
         </div>
      );

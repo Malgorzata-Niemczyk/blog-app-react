2f301c2a7ae375fd74700fa9e9911c8211084736
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-02-16 18:52:47 +0100

    create the content for the BlogDetails: reuse the useFetch hook to fetch the data for each blog and output them in the JSX template, add conditional templating for an error message and when the data is being loaded

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,10 +1,32 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const { id } = useParams();
+    const { data: blog, isPending, error } = useFetch(`http://localhost:8000/blogs/${id}`);
+
     return ( 
         <div className="blog__details">
-            <h2>Blog Details - { id }</h2>
+            {error && <div className="error__message">{ error }</div>}
+            {isPending && <div className="loading__message">Loading...</div>}
+            {blog && (
+                <>
+                    <header>
+                        <div className="header__bg" style={{
+                            backgroundImage: `url(${ blog.image })`
+                        }}>
+                            <div className="hero__bg"></div>
+                            <div className="header__text">
+                                <h2>{ blog.title }</h2>
+                                <p>{ blog.date } | Written by { blog.author }</p>
+                            </div>
+                        </div>
+                    </header>
+                    <article>
+                        <p>{ blog.body }</p>
+                    </article>
+                </>
+            )}
         </div>
      );
 }

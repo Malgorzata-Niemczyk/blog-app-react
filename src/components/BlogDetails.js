ebb8985a2b59217b3e509468c5fb2385193ede2d
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-02-16 19:04:34 +0100

    adjust the indent in the JSX template of the BlogDetails component

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -10,22 +10,22 @@ const BlogDetails = () => {
             {error && <div className="error__message">{ error }</div>}
             {isPending && <div className="loading__message">Loading...</div>}
             {blog && (
-                <>
-                    <header>
-                        <div className="header__bg" style={{
-                            backgroundImage: `url(${ blog.image })`
-                        }}>
-                            <div className="hero__bg"></div>
-                            <div className="header__text">
-                                <h2>{ blog.title }</h2>
-                                <p>{ blog.date } | Written by { blog.author }</p>
-                            </div>
+            <>
+                <header>
+                    <div className="header__bg" style={{
+                         backgroundImage: `url(${ blog.image })`
+                    }}>
+                        <div className="hero__bg"></div>
+                        <div className="header__text">
+                            <h2>{ blog.title }</h2>
+                            <p>{ blog.date } | Written by { blog.author }</p>
                         </div>
-                    </header>
-                    <article>
-                        <p>{ blog.body }</p>
-                    </article>
-                </>
+                    </div>
+                </header>
+                <article>
+                    <p>{ blog.body }</p>
+                </article>
+            </>
             )}
         </div>
      );

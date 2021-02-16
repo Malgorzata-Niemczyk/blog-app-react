f20348e27bdd00c9be76e998f9a6547e0c37466f
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-02-16 20:41:54 +0100

    create the structure and content for the form in the CreateBlog component

--- src/components/CreateBlog.js
+++ src/components/CreateBlog.js
@@ -2,6 +2,26 @@ const CreateBlog = () => {
     return ( 
         <div className="create__blog">
             <h2>Add a New Blog</h2>
+            <form>
+                <label>Blog title:</label>
+                <input 
+                    type="text" 
+                    required
+                />
+                <label> Blog body:</label>
+                <textarea 
+                    required
+                ></textarea>
+                <label>Date:</label>
+                <input type="date" required/>
+                <label>Blog author:</label>
+                <select>
+                    <option value="Jane Doe"></option>
+                    <option value="John White"></option>
+                    <option value="Lauren Smith"></option>
+                </select>
+                <button type="submit">Add Blog</button>
+            </form>
         </div>
      );
 }

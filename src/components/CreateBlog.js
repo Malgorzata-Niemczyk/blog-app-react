b533e8148216459146f24d1ae91f24732cd28d75
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-02-17 15:26:00 +0100

    create states for the input elements and set the onChange function on them to track what the user inserts in the input elements; change the value of the type attribute from the date to text to allow the user write the date in their chosen format

--- src/components/CreateBlog.js
+++ src/components/CreateBlog.js
@@ -1,24 +1,43 @@
+import { useState } from 'react';
+
 const CreateBlog = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [date, setDate] = useState('');
+    const [author, setAuthor] = useState('Jane Doe');
+
     return ( 
         <div className="create__blog">
             <h2>Add a New Blog</h2>
             <form>
                 <label>Blog title:</label>
                 <input 
-                    type="text" 
+                    type="text"
+                    value={title}
+                    onChange={event => setTitle(event.target.value)}
                     required
                 />
                 <label> Blog body:</label>
-                <textarea 
+                <textarea
+                    value={body}
+                    onChange={event => setBody(event.target.value)}
                     required
                 ></textarea>
                 <label>Date:</label>
-                <input type="date" required/>
+                <input 
+                    type="text"
+                    value={date}
+                    onChange={event => setDate(event.target.value)}
+                    required
+                />
                 <label>Blog author:</label>
-                <select>
-                    <option value="Jane Doe"></option>
-                    <option value="John White"></option>
-                    <option value="Lauren Smith"></option>
+                <select
+                    value={author}
+                    onChange={event => setAuthor(event.target.value)}
+                >
+                    <option value="Jane Doe">Jane Doe</option>
+                    <option value="John White">John White</option>
+                    <option value="Lauren Smith">Lauren Smith</option>
                 </select>
                 <button type="submit">Add Blog</button>
             </form>
